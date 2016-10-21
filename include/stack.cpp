137311df309933e3bd813c2b7610de3c56a33fb2
Balanar300hp <kudashov.vlad@yandex.ru>
2016-10-21 13:48:20 +0400

    Update stack.cpp

--- include/stack.cpp
+++ include/stack.cpp
@@ -12,7 +12,7 @@ void construct(T1 * ptr, T2 const & value) {
 }
 
 template <typename T>
-void destroy(T * ptr) noexcept 
+void destroy(T * ptr) noexcept
 {
 	ptr->~T();
 }
@@ -62,12 +62,12 @@ public:
 	stack(size_t size=0); /* noexcept */
 	stack(const stack&); /* strong */
 	~stack(); /* noexcept */
-	size_t count() const; /* noexcept */
+	size_t count() const noexcept; /* noexcept */
 	void push(T const &); /* strong */
 	const T& top(); /* strong */
 	void pop(); /* strong */
 	stack& operator=(const stack&); /* strong */
-	bool empty(); /* noexcept */
+	bool empty() noexcept; /* noexcept */
 };
 
 /*template<typename T>
@@ -81,53 +81,69 @@ return p2;
 template <typename T>
 stack<T>::stack(size_t size) : allocator<T>(size){};
 
-template <typename T>
-stack<T>::stack(const stack& x) : allocator<T>(x.size_){
-	for (size_t i = 0; i < x.count_; i++) construct(allocator<T>::ptr_ + i, x.ptr_[i]);
-	allocator<T>::count_ = x.count_;
+template <typename T>// + 
+stack<T>::stack(const stack& x){
+	allocator<T>::count_ = tmp.count_;
+	allocator<T>::ptr_ = mem_copy(tmp.count_, tmp.size_, tmp.ptr_);
+	allocator<T>::size_ = tmp.size_;
 };
 
-template <typename T>
+template <typename T>//+
 stack<T>::~stack(){ destroy(allocator<T>::ptr_, allocator<T>::ptr_ + allocator<T>::count_); }
 
-template <typename T>
-size_t stack<T>::count()const { return allocator<T>::count_; }
+template <typename T>//+
+size_t stack<T>::count()const noexcept { return allocator<T>::count_; }
 
-template <typename T>
+template <typename T>//добавилась ф-я мем_копи для копирования вместо цикла //+
+auto mem_copy(size_t count_m, size_t array_size_m, const T * tmp)->T* {
+	T *mass = new T[array_size_m];
+	try {
+	std::copy(tmp, tmp + count_m, mass);}
+	catch (...){
+	delete[] mass;
+		throw;
+	}
+	return mass;
+}
+template <typename T>//+
 void stack<T>::push(T const &a){
 	if (allocator<T>::count_ == allocator<T>::size_) {
 		size_t array_size = allocator<T>::size_ * 2 + (allocator<T>::size_ == 0);
-		stack<T> temp(array_size);
-		while (temp.count() < allocator<T>::count_) temp.push(allocator<T>::ptr_[temp.count()]); 
-		this->swap(temp);
+		T *tmp = mem_copy(allocator<T>::count_, array_size, allocator<T>::ptr_);
+		delete[] allocator<T>::ptr_;
+		allocator<T>::ptr_ = tmp;
+		allocator<T>::size_ = array_size;
+		
 	}
 	construct(allocator<T>::ptr_ + allocator<T>::count_, a);
 	++allocator<T>::count_;
 }
 
-template <typename T>
+template <typename T>//+
 const T& stack<T>::top(){
 	if (allocator<T>::count_ > 0) return allocator<T>::ptr_[allocator<T>::count_ - 1];
 	else throw("stack pust");
 }
 
-template <typename T>
-void stack<T>::pop(){
-	if (allocator<T>::count_ > 0) { --allocator<T>::count_; destroy(&(allocator<T>::ptr_[allocator<T>::count_])); }
+template <typename T>//+
+void stack<T>::pop(){//сначала очистка памяти затем уменьшение count_
+	if (allocator<T>::count_ > 0) { destroy(&(allocator<T>::ptr_[allocator<T>::count_]));  --allocator<T>::count_; }
 	else throw("stack pust");
 }
 
-template<typename T>
+template<typename T>//+
 stack<T>& stack<T>::operator=(const stack& b){
 	if (this != &b){
-		stack<T> temp(b.size_);
-		while (temp.count_ < b.count_){ construct(temp.ptr_ + temp.count_, b.ptr_[temp.count_]); ++temp.count_; }
-		this->swap(temp);
+		T* cp = mem_copy(tmp.count_, tmp.size_, tmp.ptr_);
+		delete[] allocator<T>::ptr_; 
+		allocator<T>::ptr_ = cp;
+		allocator<T>::size_ =tmp.size_;
+		allocator<T>::count_ = tmp.count_;
 	}
 	return *this;
 }
 
-template<typename T>
-bool stack<T>::empty(){ return(allocator<T>::count_ == 0); }
+template<typename T>//+
+bool stack<T>::empty() noexcept { return(allocator<T>::count_ == 0); }
 
 #endif
